46d434ef2b704e7d2cb9753874f9615c6287f42a
testhxj <51729763+testhxj@users.noreply.github.com>
2020-05-07 18:05:34 +0800

    test

---
+++ TestCase/My_tool.py
@@ -0,0 +1,241 @@
+# -*- encoding=utf8 -*-
+__author__ = "Administrator"
+#
+# from airtest.core.api import *
+#
+# auto_setup(__file__)
+#
+# from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+# poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
+
+
+#返回
+closeBtn = "cn.chengyu.love:id/closeBtn"
+backImageView = "cn.chengyu.love:id/backImageView"
+
+
+#身份认证的按钮
+idCardLay = "cn.chengyu.love:id/idCardLay"
+realNameEt ="cn.chengyu.love:id/realNameEt"
+cardNoEt = "cn.chengyu.love:id/cardNoEt"
+submitBtn = "cn.chengyu.love:id/submitBtn"
+
+#实名认证的按钮
+nameLay = "cn.chengyu.love:id/nameLay"
+#身份证正面
+frontCardImg = "cn.chengyu.love:id/frontCardImg"
+#从手机相册选择
+dialog_tv_select = "cn.chengyu.love:id/dialog_tv_select"
+#拍照
+dialog_tv_takePhoto = "cn.chengyu.love:id/dialog_tv_takePhoto"
+#取消
+dialog_tv_cancel = "cn.chengyu.love:id/dialog_tv_cancel"
+#选择图片
+check_view = "cn.chengyu.love:id/check_view"
+#使用
+button_apply = "cn.chengyu.love:id/button_apply"
+#身份证反面
+backCardImg = "cn.chengyu.love:id/backCardImg"
+#手持身份证
+takeCardImg = "cn.chengyu.love:id/takeCardImg"
+#确定按钮
+confirmBtn = "cn.chengyu.love:id/confirmBtn"
+
+#我的动态
+#点击进入动态界面
+trendsMoreImageView = "cn.chengyu.love:id/trendsMoreImageView"
+#点击发布动态
+addTrendsLayout = "cn.chengyu.love:id/addTrendsLayout"
+#选择图片
+tvCheck = "cn.chengyu.love:id/tvCheck"
+#Ok
+picture_tv_ok = "cn.chengyu.love:id/picture_tv_ok"
+#已使用
+right_shortcut = "com.android.systemui:id/right_shortcut"
+
+addImageView = "cn.chengyu.love:id/addImageView"
+#发布
+submitTextView = "cn.chengyu.love:id/submitTextView"
+#选择话题
+trendsMoreImageView = "cn.chengyu.love:id/trendsMoreImageView"
+#添加地点
+addressTextView = "cn.chengyu.love:id/addressTextView"
+#选择地点
+titleTextView = "cn.chengyu.love:id/titleTextView"
+#选择摄像按钮
+tvCamera = "cn.chengyu.love:id/tvCamera"
+#选择录视频按钮
+picture_tv_video = "cn.chengyu.love:id/picture_tv_video"
+#录制视频
+shutter_button = "com.android.camera:id/shutter_button"
+#确认视频 
+btn_done = "com.android.camera:id/btn_done"
+#添加文字
+contentEditText = "cn.chengyu.love:id/contentEditText"
+
+#基本资料
+basicInfoLay = "cn.chengyu.love:id/basicInfoLay"
+#交友心声
+singelSignTv = "cn.chengyu.love:id/singelSignTv"
+#修改昵称
+nickNameLay = "cn.chengyu.love:id/nickNameLay"
+#输入框
+signEt = "cn.chengyu.love:id/signEt"
+#出生年月
+birthDayLay = "cn.chengyu.love:id/birthDayLay"
+#选择年份
+year_pv = "cn.chengyu.love:id/year_pv"
+#学历
+educationLay = "cn.chengyu.love:id/educationLay"
+#婚姻状况
+marriedStatusLay = "cn.chengyu.love:id/marriedStatusLay"
+#身高
+heightLay = "cn.chengyu.love:id/heightLay"
+#月收入
+incomeLay = "cn.chengyu.love:id/incomeLay"
+#职业
+professionLay = "cn.chengyu.love:id/professionLay"
+#住房情况
+houseLay = "cn.chengyu.love:id/houseLay"
+#魅力部位
+charmingLay = "cn.chengyu.love:id/charmingLay"
+#魅力值
+charmCb = "cn.chengyu.love:id/charmCb"
+#血型
+bloodTypeLay = "cn.chengyu.love:id/bloodTypeLay"
+#婚后与父母同住
+liveWithParentLay = "cn.chengyu.love:id/liveWithParentLay"
+#婚前同居
+cohabitationLay = "cn.chengyu.love:id/cohabitationLay"
+
+#征友条件  
+#进入征友条件
+conditionLay = "cn.chengyu.love:id/conditionLay"
+#所在地
+locationLay = "cn.chengyu.love:id/locationLay"
+locationChooser = "cn.chengyu.love:id/locationChooser"
+#年龄范围
+ageRangeLay = "cn.chengyu.love:id/ageRangeLay"
+#最小年龄
+startAgeChooser = "cn.chengyu.love:id/startAgeChooser"
+#最大年龄
+endAgeChooser = "cn.chengyu.love:id/endAgeChooser"
+#身高
+heightLay = "cn.chengyu.love:id/heightLay"
+#最小身高
+startHeightChooser = "cn.chengyu.love:id/startHeightChooser"
+#最大身高
+endHeightChooser = "cn.chengyu.love:id/endHeightChooser"
+#最低学历
+lowestEduLay = "cn.chengyu.love:id/lowestEduLay"
+#月收入
+incomeLay = "cn.chengyu.love:id/incomeLay"
+
+#我的标签
+tagLay = "cn.chengyu.love:id/tagLay"
+
+#分享
+#邀请用户
+inviteLay = "cn.chengyu.love:id/liwuLay"
+
+#我的玫瑰
+roseLay = "cn.chengyu.love:id/roseLay"
+#微信支付按钮
+wxPayLay = "cn.chengyu.love:id/wxPayLay"
+#微信的退出按钮
+m1 = "com.tencent.mm:id/m1"
+#充值金额
+buyRose1 = "cn.chengyu.love:id/buyRose1"
+buyRose6 = "cn.chengyu.love:id/buyRose6"
+buyRose30 = "cn.chengyu.love:id/buyRose30"
+buyRose108 = "cn.chengyu.love:id/buyRose108"
+buyRose288 = "cn.chengyu.love:id/buyRose288"
+buyRose500 = "cn.chengyu.love:id/buyRose500"
+buyRose1000 = "cn.chengyu.love:id/buyRose1000"
+buyRose2000 = "cn.chengyu.love:id/buyRose2000"
+buyRose3000 = "cn.chengyu.love:id/buyRose3000"
+
+#vip 
+#进入vip界面按钮
+vipLay = "cn.chengyu.love:id/vipLay"
+#头像右侧的成为会员
+buyVipTv = "cn.chengyu.love:id/buyVipTv"
+#查看免费卡
+checkCardTv = "cn.chengyu.love:id/checkCardTv"
+#领取相亲卡
+receiverCardTv ="cn.chengyu.love:id/receiverCardTv"
+#领取好友卡
+receiverCardTv2 = "cn.chengyu.love:id/receiverCardTv2"
+#分享得会员的跳转
+adverImg = "cn.chengyu.love:id/adverImg"
+#成为会员
+confirmBtn = "cn.chengyu.love:id/confirmBtn"
+#vip类型
+vip1Lay = "cn.chengyu.love:id/vip1Lay"
+vip2Lay = "cn.chengyu.love:id/vip2Lay"
+vip3Lay = "cn.chengyu.love:id/vip3Lay"
+#续费vip类型
+vipPriceTv1 = "cn.chengyu.love:id/vipPriceTv1"
+vipPriceTv2 = "cn.chengyu.love:id/vipPriceTv2"
+vipPriceTv3 = "cn.chengyu.love:id/vipPriceTv3"
+#充值按钮
+confirmBtn = "cn.chengyu.love:id/confirmBtn"
+
+#我的钱包
+walletLay = "cn.chengyu.love:id/walletLay"
+
+#投诉与反馈按钮
+adviceLay = "cn.chengyu.love:id/adviceLay"
+#文字输入框
+chat_message_input = "cn.chengyu.love:id/chat_message_input"
+#发送按钮
+send_btn = "cn.chengyu.love:id/send_btn"
+#表情按钮
+face_btn = "cn.chengyu.love:id/face_btn"
+#添加照片的按钮
+more_btn = "cn.chengyu.love:id/more_btn"
+#相册的图标
+imageView = "cn.chengyu.love:id/imageView"
+
+#设置
+settingLay = "cn.chengyu.love:id/settingLay"
+#黑名单
+blackListLay = "cn.chengyu.love:id/blackListLay"
+#退出按钮
+logoutBtn = "cn.chengyu.love:id/logoutBtn"
+
+'''
+def backward():    
+    while poco(name = closeBtn).exists():
+        poco(name = closeBtn).click()
+        
+def backward1():    
+    while poco(name = backImageView).exists():
+        poco(name = backImageView).click()
+
+A = "android.widget.LinearLayout"
+B = "android:id/content"
+C = "android.widget.FrameLayout"
+D = "android.widget.LinearLayout"
+E = "cn.chengyu.love:id/tabHost"
+F = "android.widget.LinearLayout"
+G = "android.widget.LinearLayout"
+
+
+def cheng_yu_button():
+    poco(A).offspring(B).child(C).child(D).child(E).child(F).child(G)[0].click()
+
+def blind_date_button():
+    poco(A).offspring(B).child(C).child(D).child(E).child(F).child(G)[1].click()
+    
+def zone_button():
+    poco(A).offspring(B).child(C).child(D).child(E).child(F).child(G)[2].click()
+    
+def im_button():
+    poco(A).offspring(B).child(C).child(D).child(E).child(F).child(G)[3].click()
+    
+def my_button():
+    poco(A).offspring(B).child(C).child(D).child(E).child(F).child(G)[4].click()
+
+'''
+
